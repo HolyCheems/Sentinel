9c4182c5b354d75f0707b6d1285d1386a9148104
chasevaughn13 <chasevaughn13@gmail.com>
2019-11-18 22:02:13 -0600

    Removed unnecessary test files

--- EagleEye/Global.asax.cs
+++ EagleEye/Global.asax.cs
@@ -17,7 +17,6 @@ namespace EagleEye
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
 			EagleEyeConfig.ImportDatabase();
-			Tests.Tests.RunTests();
 		}
 		~MvcApplication()
 		{
